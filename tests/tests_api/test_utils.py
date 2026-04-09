fd45cea1ab63f6128803e171e641f815d8cf1a7e
annadiarov <annadiarov@gmail.com>
2026-04-09 16:47:15 +0200

    test: Update TestUtils to use new hash-based subdirectory structure

--- tests/tests_api/test_utils.py
+++ tests/tests_api/test_utils.py
@@ -1,11 +1,14 @@
 from bio3dbeacons.api import utils
+from bio3dbeacons.hashpath import get_hash_subdir
 
 
 class TestUtils:
     def test_get_model_asset_url_default(self):
+        subdir = get_hash_subdir("someEntryId")
         result = utils.get_model_asset_url("someEntryId")
-        assert result == "localhost/static/cif/someEntryId.cif"
+        assert result == f"static/{subdir}/cif/someEntryId.cif"
 
     def test_get_model_asset_url_pdb(self):
+        subdir = get_hash_subdir("someEntryId")
         result = utils.get_model_asset_url("someEntryId", "pdb")
-        assert result == "localhost/static/pdb/someEntryId.pdb"
+        assert result == f"static/{subdir}/pdb/someEntryId.pdb"
